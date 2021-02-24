f2fc197b52f5efffefa8dffc2fc9aa37d604f4ae
mariaolmedilla <mariaolmedilla1@gmail.com>
2021-02-24 09:56:30 +0100

    Change count.py

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,1 +1,2 @@
 print("Hello world. This is my first Pycharm file")
+print("This is my first practice ")
---
+++ Session-01/sum20.py
@@ -0,0 +1,13 @@
+# Session 1. Exercise 3
+# Calculate the sum of the 20 first integer numbers
+# 1 + 2 + 3 + ... + 20
+# Example wprking!
+
+# -- Store the result
+res = 0
+
+for i in range(1,21):
+    # print("Now res is equal to", res)
+    res += 1
+
+print("Total sum: ", res)
