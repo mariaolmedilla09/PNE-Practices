39934ef608137c8718972934adcbad4ed8f3f68f
mariaolmedilla <mariaolmedilla1@gmail.com>
2021-03-09 09:12:24 +0100

    1st commit

---
+++ Session-06/Dog.py
@@ -0,0 +1,7 @@
+
+
+class Dog:
+    def __init__(self, name, age):  # init will create one instance of the class. This function is called a "constructor of the class" and it is executed every time we create an object of the create class.
+        self.name = name
+        self.age = age
+    pass
