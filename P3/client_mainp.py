5c72d4f36da000cfeb5d583038f1619a3449813d
mariaol <mariaolmedilla1@gmail.com>
2021-04-16 19:45:24 +0200

    commited

--- P3/client_mainp.py
+++ P3/client_mainp.py
@@ -2,18 +2,21 @@
 
 from Client0 import Client
 
+gene_list = {"ADA", "FRAT1", "FXN", "RNU6_269P", "U5"}
+
 IP = "127.0.0.1"
 PORT = 8080
 
 client = Client(IP, PORT)
 command = input("Type the command you want to execute: ")
+
 if command == "PING":
     print(" * Testing PING...")
     client.talk(command)
 elif command == "GET":
     print(" * Testing GET...")
-    argument = input("Enter an argument: ")
-    client.talk(command + " " + argument)
+    for a in range(0,5):
+        client.talk(command + " " + str(a))
 elif command == "INFO":
     print(" * Testing INFO...")
     client.talk(command + " " + "AGCTGATCGATGGGTAGCATGCT")
@@ -25,6 +28,6 @@ elif command == "REV":
     client.talk(command + " " + "AGCTGATCGATGGGTAGCATGCT")
 elif command == "GENE":
     print(" * Testing GENE...")
-    argument = input("Enter an argument: ")
-    client.talk(command + " " + argument)
+    for i in gene_list:
+        client.talk(command + " " + i)
 
